ff35f0dda1da967392ba0c7927a2b0fc515b47e0
oliverlaudan-ops <oliver.laudan@gmail.com>
2025-12-17 13:07:33 +0100

    Update prestige-upgrades.js

--- src/modules/prestige-upgrades.js
+++ src/modules/prestige-upgrades.js
@@ -1,3 +1,35 @@
 // Prestige Upgrade-Definitionen
-export class PrestigeUpgrade{};
-export default [];
+// src/modules/prestige-upgrades.js
+
+// Basis-Klasse für Prestige-Upgrades (Platzhalter)
+export class PrestigeUpgrade {
+  constructor({ id = '', name = '', desc = '', cost = 0, apply = () => {} } = {}) {
+    this.id = id;
+    this.name = name;
+    this.desc = desc;
+    this.cost = cost;
+    this.level = 0;
+    this.applyFn = apply;
+  }
+
+  canBuy(gameState) {
+    // prestige-Punkte liegen z.B. in gameState.prestige
+    return (gameState.prestige || 0) >= this.cost;
+  }
+
+  buy(gameState) {
+    if (!this.canBuy(gameState)) return false;
+    gameState.prestige = (gameState.prestige || 0) - this.cost;
+    this.level++;
+    if (typeof this.applyFn === 'function') {
+      this.applyFn(gameState);
+    }
+    return true;
+  }
+}
+
+// Liste aller Prestige-Upgrades (vorerst leer)
+const prestigeUpgradesList = [];
+
+// Standard-Export für core.js
+export default prestigeUpgradesList;
